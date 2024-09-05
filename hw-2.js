dda83aa4c18357c89f9f6b769fb0f2e42caf5be8
40smilana <40smilana@gmail.com>
2024-09-05 21:05:31 +0300

    все задания выполнила

--- hw-2.js
+++ hw-2.js
@@ -1,16 +1,60 @@
 /* задание 1 */
 let a = 10;
 alert(a);
-
 a = 20;
 alert(a);
 
 /* задание 2 */
+const yearOfReleaseIphone = 2007;
+alert(yearOfReleaseIphone);
+
 /* задание 3 */
+const jsCreator = "Брендан Эйх";
+alert(jsCreator);
+
 /* задание 4 */
+let ten = 10;
+let two = 2;
+let sum = ten + two;
+let difference = ten - two;
+let product = ten * two;
+let quotient = ten / two;
+alert(sum);
+alert(difference);
+alert(product);
+alert(quotient);
+
 /* задание 5 */
+let result = 2 ** 5;
+alert(result);
+
 /* задание 6 */
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert(remainder);
+
 /* задание 7 */
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
 /* задание 8 */
+let age = prompt("Сколько вам лет?");
+alert(age);
+
 /* задание 9 */
+const user = {
+name: 'Шапокляк',
+age: 73,
+isAdmin: true
+}
+
 /* задание 10 */
+const name = prompt ("Как тебя зовут?");
+alert(`Привет, ${name}`)
