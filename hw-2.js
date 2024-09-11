db80004f655d57d4a5dd42b76ff3777310408f8e
40smilana <40smilana@gmail.com>
2024-09-12 02:45:15 +0300

    Сделала верстку + выполнила задания по js в файле hw-3.js

--- hw-2.js
+++ hw-2.js
@@ -1,18 +1,18 @@
-/* задание 1 */
+
 let a = 10;
 alert(a);
 a = 20;
 alert(a);
 
-/* задание 2 */
+
 const yearOfReleaseIphone = 2007;
 alert(yearOfReleaseIphone);
 
-/* задание 3 */
+
 const jsCreator = "Брендан Эйх";
 alert(jsCreator);
 
-/* задание 4 */
+
 let ten = 10;
 let two = 2;
 let sum = ten + two;
@@ -24,17 +24,17 @@ alert(difference);
 alert(product);
 alert(quotient);
 
-/* задание 5 */
+
 let result = 2 ** 5;
 alert(result);
 
-/* задание 6 */
-let a = 9;
-let b = 2;
-let remainder = a % b;
+
+let aa = 9;
+let bb = 2;
+let remainder = aa % bb;
 alert(remainder);
 
-/* задание 7 */
+
 let num = 1;
 num += 5;
 num -= 3;
@@ -44,17 +44,17 @@ num++;
 num--;
 alert(num);
 
-/* задание 8 */
+
 let age = prompt("Сколько вам лет?");
 alert(age);
 
-/* задание 9 */
+
 const user = {
 name: 'Шапокляк',
-age: 73,
+agee: 73,
 isAdmin: true
 }
 
-/* задание 10 */
-const name = prompt ("Как тебя зовут?");
-alert(`Привет, ${name}`)
+
+const namee = prompt ("Как тебя зовут?");
+alert(`Привет, ${namee}`)
